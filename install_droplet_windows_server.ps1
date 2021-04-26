af909af39ea14c117c7eb1393362c50f1064d001
idanre1 <idanre1@gmx.com>
2021-04-26 12:40:41 +0300

    Update install_droplet_windows_server.ps1

--- install_droplet_windows_server.ps1
+++ install_droplet_windows_server.ps1
@@ -24,7 +24,15 @@ $profiledomain=$fwMgr.LocalPolicy.GetProfileByType(0)
 $profiledomain.GloballyOpenPorts.Add($port1)
 
 #==============================================================
-# Creates a rule to open an incomming port in the firewall.
+# Creates a NAT switch
+#==============================================================
+New-VMSwitch -SwitchName "NATSwitch" -SwitchType Internal
+New-NetIPAddress -IPAddress 192.168.10.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
+New-NetNAT -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.10.0/24
+Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/24" -ExternalPort 22 -Protocol TCP -InternalIPAddress "192.168.10.2" -InternalPort 22 -NatName NATNetwork
+
+#==============================================================
+# VM AAD setup
 #==============================================================
 #device-manager->goto display driver location path: copy the PCIE details:
 # dont forget to disable the device
