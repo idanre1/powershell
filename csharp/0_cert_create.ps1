9acf06b934bb2a52755c6bdf91a79b6a3df0c67a
Idan Regev <iregev@microsoft.com>
2024-02-04 12:13:33 +0200

    specific name for testing

--- csharp/0_cert_create.ps1
+++ csharp/0_cert_create.ps1
@@ -1,5 +1,5 @@
 # new certificate
-New-SelfSignedCertificate -Type Custom -Subject "CN=Contoso Software, O=Contoso Corporation, C=US" -KeyUsage DigitalSignature -FriendlyName "Your friendly name goes here" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+New-SelfSignedCertificate -Type Custom -Subject "CN=iregev" -KeyUsage DigitalSignature -FriendlyName "AppTestForDebug" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
 
 # make sure its there
 Set-Location Cert:\CurrentUser\My
