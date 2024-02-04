3c90ca8954bc734e727e98ded88d9b61a033cd15
Idan Regev <iregev@microsoft.com>
2024-02-04 12:30:29 +0200

    with export

--- csharp/2_cert_import.ps1
+++ csharp/2_cert_import.ps1
@@ -1,9 +1,10 @@
+# !!! Dont forget to import certificate to the user store !!!
 
 # appbundle
-$app=args[0]
+$app=$args[0]
 
 # signtool location
 New-Item -Path Alias:signtool -Value C:\"Program Files (x86)"\"Windows Kits"\10\bin\10.0.22621.0\x64\signtool.exe
 
 # sign
-signtool sign /fd /a SHA256 $app
+signtool sign /fd SHA256 /a /f test_cert_${user}.pfx $app
